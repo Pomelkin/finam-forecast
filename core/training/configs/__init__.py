7028d15adb947bfe52eaf62c85da7b8f3337650c
pomelk1n <emelya.kara@yandex.ru>
2025-10-07 14:43:43 +0500

    refactor(configs): remove ModelProperties class
    - Delete the unused ModelProperties class and its associated model_properties.py file
    - Remove related imports and exports from the configs module's __init__.py
    - This simplifies the configuration structure by eliminating obsolete code that defined model properties like encoders and normalization settings

--- core/training/configs/__init__.py
+++ core/training/configs/__init__.py
@@ -1,7 +1,6 @@
 from .config_mixins import ConfigMixin
 from .hyperparams import Hyperparams
 from .hyperparams import Lr
-from .model_properties import ModelProperties
 from .training_params import DataConfig
 from .training_params import FSDPStrategyConfig
 from .training_params import SingleDeviceStrategyConfig
@@ -9,7 +8,6 @@ from .training_params import TrainingParams
 
 __all__ = [
     "Hyperparams",
-    "ModelProperties",
     "TrainingParams",
     "Lr",
     "ConfigMixin",
